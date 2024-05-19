d2232ab8b40d833b0f998352f34d3f0018300ecc
KhanSul <suleimanakhmedovi@gmail.com>
2024-05-19 19:33:27 +0400

    Add files via upload

---
+++ src/pages/Main.jsx
@@ -0,0 +1,167 @@
+import './Main.css'
+
+export default function Main() {
+  return (
+    <div className="main">
+      <div className="container-15">
+      </div>
+      <div className="container-16">
+      </div>
+      <div className="container-18">
+      </div>
+      <div className="container-5">
+        <div className="group-3">
+          <div className="container-7">
+            <div className="container-11">
+              <div className="group-1">
+                <div className="meta-l-1">
+                MetaL
+                </div>
+                <div className="e-3">
+                e
+                </div>
+                <div className="ath-1">
+                ath
+                </div>
+                <div className="e-2">
+                e
+                </div>
+                <div className="r-1">
+                r
+                </div>
+              </div>
+              <div className="soon-1">
+              Soon
+              </div>
+            </div>
+            <div className="container-19">
+            </div>
+          </div>
+          <div className="container-10">
+            <div className="container-20">
+            </div>
+            <div className="container-21">
+            </div>
+            <div className="container-22">
+            </div>
+          </div>
+        </div>
+        <div className="group-4">
+          <div className="container">
+          </div>
+          <div className="container-6">
+            <div className="container-2">
+              <div className="container-3">
+              </div>
+              <span className="belts">
+              belts
+              </span>
+            </div>
+            <div className="container-4">
+              <div className="container-3">
+                <div className="container-9">
+                  <div className="container-2">
+                  </div>
+                  <div className="container-4">
+                  </div>
+                </div>
+                <div className="container-5">
+                </div>
+              </div>
+              <div className="container-1">
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="care">
+        Care
+        </div>
+        <div className="as-leather-ages-it-undergoes-anatural-process-of-transformation-gaining-character-and-patina-over-time-exposure-to-elements-like-sunlight-moisture-and-dirt-can-cause-the-leather-to-lose-its-suppleness-and-develop-cracks-or-dullness-without-proper-care-this-beloved-material-can-lose-its-charm-leaving-owners-dismayed-by-its-diminished-appearance-enter-our-premium-leather-balm-meticulously-crafted-to-revive-and-protect-your-cherished-leather-possessions-infused-with-nourishing-ingredients-our-balm-restores-moisture-and-enhances-the-natural-luster-of-leather-effectively-combating-signs-of-aging-with-regular-application-our-balm-not-only-preserves-the-integrity-of-your-leather-goods-but-also-prolongs-their-lifespan-ensuring-they-continue-to-exude-timeless-elegance-for-years-to-come-treat-your-leather-right-with-our-trusted-balm-because-every-piece-deserves-to-age-gracefully">
+        As leather ages, it undergoes a natural process of transformation, gaining character and patina over time. Exposure to elements like sunlight, moisture, and dirt can cause the leather to lose its suppleness and develop cracks or dullness. Without proper care, this beloved material can lose its charm, leaving owners dismayed by its diminished appearance.<br />
+        <br />
+        Enter our premium leather balm, meticulously crafted to revive and protect your cherished leather possessions. Infused with nourishing ingredients, our balm restores moisture and enhances the natural luster of leather, effectively combating signs of aging. With regular application, our balm not only preserves the integrity of your leather goods but also prolongs their lifespan, ensuring they continue to exude timeless elegance for years to come. Treat your leather right with our trusted balm – because every piece deserves to age gracefully.
+        </div>
+        <div className="group-5">
+          <div className="container-8">
+          </div>
+          <div className="container-8">
+            <div className="container-13">
+              <span className="wallets">
+              Wallets
+              </span>
+              <div className="container-10">
+              </div>
+            </div>
+            <div className="container">
+              <div className="container-6">
+              </div>
+              <div className="container-7">
+              </div>
+              <div className="container-9">
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="blog">
+        BLOG
+        </div>
+        <div className="tanning-processes-for-superior-wallet-quality">
+        tanning processes for superior wallet quality
+        </div>
+        <div className="leather-tanning-is-the-process-of-treating-animal-hides-to-produce-durable-and-flexible-leather-the-primary-purpose-of-tanning-is-to-stabilize-the-collagen-fibers-in-the-hide-making-it-resistant-to-decomposition-and-suitable-for-various-uses-there-are-several-tanning-methods-each-imparting-distinct-characteristics-to-the-leather-vegetable-tanning-is-one-of-the-oldest-and-most-traditional-methods-using-natural-tannins-derived-from-plant-sources-like-tree-bark-leaves-and-fruits-this-process-can-take-several-weeks-allowing-the-tannins-to-penetrate-deeply-into-the-hide-the-result-is-afirmer-more-rigid-leather-that-ages-beautifully-developing-arich-patina-over-time-wallets-crafted-from-vegetable-tanned-leather-are-prized-for-their-durability-and-unique-aesthetic-which-becomes-more-pronounced-with-use">
+        Leather tanning is the process of treating animal hides to produce durable and flexible leather. The primary purpose of tanning is to stabilize the collagen fibers in the hide, making it resistant to decomposition and suitable for various uses. There are several tanning methods, each imparting distinct characteristics to the leather.<br />
+        <br />
+        Vegetable tanning is one of the oldest and most traditional methods, using natural tannins derived from plant sources like tree bark, leaves, and fruits. This process can take several weeks, allowing the tannins to penetrate deeply into the hide. The result is a firmer, more rigid leather that ages beautifully, developing a rich patina over time. Wallets crafted from vegetable-tanned leather are prized for their durability and unique aesthetic, which becomes more pronounced with use.
+        </div>
+        <div className="group-6">
+          <div className="container-11">
+          </div>
+          <div className="container-1">
+            <div className="container-14">
+              <div className="container-12">
+              </div>
+              <span className="long-wallets">
+              long<br />
+              Wallets
+              </span>
+              <div className="container-13">
+              </div>
+            </div>
+            <div className="container-14">
+            </div>
+          </div>
+        </div>
+        <div className="about-us">
+        About us
+        </div>
+        <div className="welcome-to-our-leather-goods-emporium-where-craftsmanship-meets-timeless-elegance-since-2019-weve-been-dedicated-to-redefining-luxury-with-the-finest-natural-leather-each-piece-is-meticulously-handcrafted-ensuring-unmatched-quality-and-authenticity-we-believe-in-the-enduring-beauty-and-durability-of-natural-leather-thats-why-we-source-only-the-finest-hides-creating-products-that-exude-luxury-and-sophistication-our-commitment-to-sustainability-means-that-when-you-choose-our-goods-youre-not-just-investing-in-style-but-also-in-responsible-consumerism-from-classic-accessories-to-modern-statement-pieces-our-collection-offers-something-for-every-taste-join-us-in-celebrating-the-beauty-of-natural-leather-and-timeless-craftsmanship-explore-our-range-today-and-experience-luxury-redefined-welcome-to-our-leather-goods-store">
+        Welcome to our leather goods emporium, where craftsmanship meets timeless elegance. Since 2019, we&#39;ve been dedicated to redefining luxury with the finest natural leather. Each piece is meticulously handcrafted, ensuring unmatched quality and authenticity.<br />
+        <br />
+        We believe in the enduring beauty and durability of natural leather. That&#39;s why we source only the finest hides, creating products that exude luxury and sophistication. Our commitment to sustainability means that when you choose our goods, you&#39;re not just investing in style, but also in responsible consumerism.<br />
+        <br />
+        From classic accessories to modern statement pieces, our collection offers something for every taste. Join us in celebrating the beauty of natural leather and timeless craftsmanship. Explore our range today and experience luxury redefined. Welcome to our leather goods store.
+        </div>
+        <div className="container-12">
+          <div className="meta-l">
+          MetaL
+          </div>
+          <div className="e">
+          e
+          </div>
+          <div className="ath">
+          ath
+          </div>
+          <div className="e-1">
+          e
+          </div>
+          <div className="r">
+          r
+          </div>
+        </div>
+        <span className="soon">
+        Soon
+        </span>
+      </div>
+    </div>
+  )
+}
